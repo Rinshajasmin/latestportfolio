3e86f4b0b435b2419d58d6dfc53979122b216521
Rinshajasmin <rinshajasmin99@gmail.com>
2026-02-06 00:46:29 +0300

    Portfolio logo created

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,14 @@ const Navbar = () => {
   return (
     <nav className='bg-transparent'>
       <div className='text-white py-5 flux justify-between items-center'>
+        <div className='text-lg font-semibold cursor-pointer'>
+                    <span className="text-white">&lt;</span>
 
+                   <span className="text-[FF8C1A ]">Portfolio</span>
+                                      <span className="text-white">/&gt;</span>
+
+
+        </div>
       </div>
     </nav>
   )
