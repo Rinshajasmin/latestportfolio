fed055a0853112d3651f97a60375343f512e6f07
Rinshajasmin <rinshajasmin99@gmail.com>
2026-02-15 03:01:42 +0300

    Navbar section created

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,133 @@
-import React from 'react'
-import {FiMenu,FiX} from 'react-icons/fi'
-import {FaGithub,FaLinkedin} from 'react-icons/fa'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { FiMenu, FiX } from "react-icons/fi";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeSection, setActiveSection] = useState("");
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const handleMenuItemClick = (sectionId) => {
+    setActiveSection(sectionId);
+    setIsOpen(false);
+  };
+  const menuItems = [
+    { id: "about", label: "About" },
+    { id: "skills", label: "Skills" },
+    { id: "education", label: "education" },
+    { id: "experience", label: "Experience" },
+    { id: "works", label: "Works" },
+  ];
+
   return (
-    <nav className='bg-transparent'>
-      <div className='text-white py-5 flux justify-between items-center'>
-        <div className='text-lg font-semibold cursor-pointer'>
-                    <span className="text-white">&lt;</span>
+    <nav
+      className={`fixed top-0 w-full z-50 transition duration-300 px-[7vw] md:px-[7vw] lg:px-[20vw] ${
+        isScrolled
+          ? "bg-[#050414] bg-opacity-50 backdrop-blur-md shadow-md"
+          : "bg-transparent"
+      }`}
+    >
+      <div className=" text-white py-5 flex justify-between items-center">
+        <div className="text-lg font-semibold cursor-pointer">
+          <span className=" font-inter font-extrabold text-3xl bg-linear-to-r from-orange-600 via-orange-500 to-[#FF8C1A] bg-clip-text text-transparent drop-shadow-md">
+            Portfolio.
+          </span>
+        </div>
+        <ul className="hidden md:flex space-x-8 text-gray-300">
+          {menuItems.map((item) => (
+            <li
+              key={item.id}
+              className={
+                'cursor-pointer hover:text-[#FF8C1A] ${activeSection === item.id?"text-[#FF8C1A]":""}'
+              }
+            >
+              <button onClick={() => handleMenuItemClick(item.id)}>
+                {item.label}
+              </button>
+            </li>
+          ))}
+        </ul>
 
-                   <span className="text-[FF8C1A ]">Portfolio</span>
-                                      <span className="text-white">/&gt;</span>
+        <div className="hidden md:flex space-x-4">
+          <a
+            href="https://github.com/Rinshajasmin"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#FF8C1A]"
+          >
+            <FaGithub size={24} />
+          </a>
+          <a
+            href="www.linkedin.com/in/rinshajasmin"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-300 hover:text-[#FF8C1A]"
+          >
+            <FaLinkedin size={24} />
+          </a>
+        </div>
 
-
+        <div className="md:hidden">
+          {isOpen ? (
+            <FiX
+              className="text-3xl text-[#FF8C1A] cursor-pointer"
+              onClick={() => setIsOpen(false)}
+            />
+          ) : (
+            <FiMenu
+              className="text-3xl text-[#FF8C1A] cursor-pointer"
+              onClick={() => setIsOpen(true)}
+            />
+          )}
         </div>
       </div>
+      {/* Mobile Menu Items */}
+      {isOpen && (
+        <div className="absolute top-16 left-1/2 transform -translate-x-1/2 w-4/5 bg-[#050414] bg-opacity-50 backdrop-filter backdrop-blur-lg z-50 rounded-lg shadow-lg md:hidden">
+          <ul className="flex flex-col items-center space-y-4 py-4 text-gray-300">
+            {menuItems.map((item) => (
+              <li
+                key={item.id}
+                className={`cursor-pointer hover:text-white ${
+                  activeSection === item.id ? "text-[#FF8C1A]" : ""
+                }`}
+              >
+                <button onClick={() => handleMenuItemClick(item.id)}>
+                  {item.label}
+                </button>
+              </li>
+            ))}
+            <div className="flex space-x-4">
+              <a
+                href="https://github.com/Rinshajasmin"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-300 hover:text-white"
+              >
+                <FaGithub size={24} />
+              </a>
+              <a
+                href="www.linkedin.com/in/rinshajasmin"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-300 hover:text-white"
+              >
+                <FaLinkedin size={24} />
+              </a>
+            </div>
+          </ul>
+        </div>
+      )}
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
